420e15932178efa0b85674661b62651e5015c4c6
alex-arraga <alex-arraga@users.noreply.github.com>
2024-03-05 02:50:19 -0300

    Improvement db validations

--- backend/db/config.go
+++ backend/db/config.go
@@ -3,37 +3,50 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func ConnectDatabase() {
+type DatabaseCollections struct {
+	Products *mongo.Collection
+}
+
+func ConnectDatabase() error {
 	// Create client connection
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
-
 	if err != nil {
-		println("🔴 Error: db connection")
-		panic(err)
+		return fmt.Errorf("failed to connect to db: %w", err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Check if the db is connect
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB: ", err)
 	}
 
-	// If not exist, I create my database and the collection
+	// I create my database (if not exist) and the collection
 	coll := client.Database("my_database").Collection("products")
 
 	// Insert bson data into collection created
-	coll.InsertOne(context.TODO(), bson.D{{
-		Key: "name", Value: "mouse",
+	newData, err := coll.InsertOne(context.TODO(), bson.D{{
+		Key: "name", Value: "laptop",
 	}})
+	if err != nil {
+		return fmt.Errorf("failed to insert into db: %w", err)
+	}
 
-	fmt.Println("Operaciones en base de datos ejecutadas")
+	fmt.Println("Insert db success: ", newData.InsertedID)
+	return nil
 }
-
-// For later use:
-
-// type DatabaseCollections struct {
-// 	Products *mongo.Collection
-// }
-
-// var Collections DatabaseCollections
-// var Client *mongo.Client
