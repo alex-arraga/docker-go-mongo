ceba1282ed5c16207470634d40a7f1fe8a8d1e59
alex-arraga <alex-arraga@users.noreply.github.com>
2024-03-24 23:12:13 -0300

    Fixed: Docker connection with backend service

--- frontend/src/hooks/useGetProducts.ts
+++ frontend/src/hooks/useGetProducts.ts
@@ -6,13 +6,16 @@ import { useEffect, useState } from "react"
 export function useGetAllProducts() {
   const [products, setProducts] = useState<ProductsApi | null>(null)
 
+  const apiURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000"
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+
   useEffect((): void => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).then(response => {
+    fetch(`${apiURL}/products`, options).then(response => {
       if (!response.ok) {
         throw new Error("Error fetching response")
       }
