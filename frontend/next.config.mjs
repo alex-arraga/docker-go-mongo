89a634933e5b84b372d57580209e61258e957f09
alex-arraga <alex-arraga@users.noreply.github.com>
2024-03-30 19:05:13 -0300

    CORS Next.js config added

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: process.env.PUBLIC_API_URL || "http://localhost:4000",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://docker-go-mongo-frontend.onrender.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   output: "standalone"
 };
 
