fbf28f81e4d6429455b107426ec96fef9fc89ad1
alex-arraga <alex-arraga@users.noreply.github.com>
2024-04-01 03:44:57 -0300

    Test deploy 6

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BACKEND_URL: process.env.RENDER_EXTERNAL_URL || "http://localhost:4000"
+    BACKEND_URL: process.env.RENDER_EXTERNAL_URL || "https://docker-go-mongo.onrender.com" || "http://localhost:4000"
   },
   output: "standalone"
 };
